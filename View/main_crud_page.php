7175342c1dd7ec8aad16be0516aedce620852468
Erick Armijos <e.armijos@yahoo.com>
2024-03-06 23:31:10 -0500

    crud header

--- View/main_crud_page.php
+++ View/main_crud_page.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/app.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>hola mundo</h1>
-    <a href="../login/logic_login/exit.php">Salir</a>
+
+<header class="header">
+        <div class="container container-header">
+            <h1>Bienvenido</h1>
+        </div>
+</header>
+
+<section class="seccion-crud">
+    
+</section>
+
+
 </body>
 </html>
